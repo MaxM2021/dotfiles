f45d075b64106ce04347102e0b3299ade2b8e155
MaxM2021 <origami.freak11@gmail.com>
2021-12-26 13:15:04 -0600

    Ranger Compression and extraction scripts

--- bin/init-dots-repo.sh
+++ bin/init-dots-repo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#run this for brand new fresh start repo
 git init --bare $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 config config --local status.showUntrackedFiles no
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
+
